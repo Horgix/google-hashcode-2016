e0f29b733cb31c12c0a8a5568ac377ba5f6dea50
Alexis 'Horgix' Chotard <alexis.horgix.chotard@gmail.com>
2016-02-11 20:32:56 +0100

    Drones: add boilerplate around products management

--- delivery.py
+++ delivery.py
@@ -1,11 +1,18 @@
 #! /usr/bin/env python3
 
+from enum import Enum
+
 class Warehouse:
     def __init__(self, nb, r, c):
         self.row = r            # Warehouse position row
         self.column = c         # Warehouse position column
         self.nb = nb            # Warehouse number (ID)
         self.products = {}      # Number of each product in stock
+    def retrieveProduct(self, productID, quantity):
+        self.products[productID] -= quantity
+        if self.products[productID] < 0:
+            raise Exception("Retrieved too much of a product !")
+
 
 class Order:
     def __init__(self, i, n, r, c):
@@ -15,13 +22,31 @@ class Order:
         self.productsNb = n     # Number of products in order
         self.products = {}      # Number of each product in order
     def getDelivered(self, productID, quantity):
-        self.products[productID] -= quantiy
+        self.products[productID] -= quantity
         if self.products[productID] == 0:
             del(self.products[productID])
         elif self.products[productID] < 0:
             raise Exception("Delivered too much of a product !")
-    def satisfied(self):
+    def isSatisfied(self):
         return self.products == {}
+    def __str__(self):
+        return "Order " + str(self.nb) + " = " + str(self.products)
+
+class DroneStatus(Enum):
+    Flying = 1
+    AtWarehouse = 2
+
+class Drone:
+    def __init__(self, n, r, c):
+        self.row = r            # Drone current row
+        self.colum = c          # Drone current column
+        self.nb = n             # Drone number (ID)
+        self.status = DroneStatus.AtWarehouse
+        self.products = {}      # Quantity of each product that the drone carries
+    def loadWith(self, productID, quantity):
+        if not productID in self.products:
+            self.products[productID] = 0
+        self.products[productID] += quantity
 
 class Map:
     def import_from_file(self, filename):
@@ -66,13 +91,20 @@ class Map:
                     raise Exception("Orders nb wrong somewhere")
                 for p in ps:
                     if not p in self.orders[i].products:
-                        self.orders[i].products[p] = 1
-                    else:
-                        self.orders[i].products[p] += 1
+                        self.orders[i].products[p] = 0
+                    self.orders[i].products[p] += 1
+                print(self.orders[i])
             # End of parsing
             remainingLines = f.readlines()
             if remainingLines != []:
                 raise Exception("Didn't parse every line")
+            # Setup drones
+            self.drones = {}
+            startr = self.warehouses[0].row
+            startc = self.warehouses[0].column
+            for i in range(self.dronesNb):
+                self.drones[i] = Drone(i, startr, startc)
+
 
     def __str__(self):
         out = ""
@@ -91,9 +123,22 @@ class Map:
         #        out += self.matrix[i][j].value
         #    out += '\n'
         return out
+    def getNumberOfSatisfiedOrders(self):
+        return len([o for onb, o in self.orders.items() if o.isSatisfied()])
+    #def processTurn(self):
+    def findWarehouseWithEnoughProducts(self, productID, quantity):
+        for wnb, w in self.warehouses.items():
+            if w.products[productID] >= quantity:
+                return wnb
+        # If we get there, the product is available nowhere
+        raise Exception("Product not available in warehouses")
+    def loadProduct(self, droneID, warehouseID, productID, quantity):
+        self.warehouses[warehouseID].retrieveProduct(productID, quantity)
+        self.drones[droneID].loadWith(productID, quantity)
 
 filename = "mother_of_all_warehouses.in"
 #filename = "busy_day.in"
 s = Map()
 s.import_from_file(filename)
 print(s)
+print(s.getNumberOfSatisfiedOrders())
